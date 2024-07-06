34af1a72481f42942cac5d2c951cf0b7259efed9
JoseVasquez1104 <profefarias80@gmail.com>
2024-07-06 18:56:39 -0400

    Proceso de Enunciado y nombre de los integrantes

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/* Integrantes: José Antonio Vásquez, Adrian Aguirre, Jesús arrieche, Luis Espinozas  */
+
+/*Realice un programa que contenga un menú de opciones y en cada opción
+ejecutara los siguientes requerimientos. Un algoritmo que calcule el factorial de un número dado. 
+Un algoritmo que calcule el valor absoluto de un número dato.
+Un algoritmo que calcule el promedio para cualquier cantidad de
+elementos dados.*/
+
+
+int main(int argc, char** argv) {
+	return 0;
+}
